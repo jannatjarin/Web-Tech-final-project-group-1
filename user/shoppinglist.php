385111471968adfc7d77dc7ef08f2fd00f91827e
anuanika21 <iamanuanika21@gmail.com>
2026-05-15 00:00:12 +0600

    get form data for add item

--- user/shoppinglist.php
+++ user/shoppinglist.php
@@ -6,3 +6,8 @@ if (!isset($_SESSION['shopping_list'])) {
 }
 
 
+if (isset($_POST['add_item'])) {
+
+    $item = trim($_POST['item']);
+    $qty = trim($_POST['qty']);
+    $unit = trim($_POST['unit']);
