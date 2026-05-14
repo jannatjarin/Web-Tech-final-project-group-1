5969c00ed6071b3b87868b98084dae9aba301bc2
anuanika21 <iamanuanika21@gmail.com>
2026-05-14 13:38:20 +0600

    change the registration controller name

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
 <body>
 
 <h2>User Registration</h2>
-<form method="post" action="registration controller.php">
+<form method="post" action="registration_controller.php">
 
 <input type="text" name="name" placeholder="Name"><br><br>
 
@@ -17,8 +17,18 @@
 <input type="password" name="password" placeholder="Password"><br><br>
 
 
-</form>
 
+
+<label>Dietary Preferences</label><br>
+
+<input type="checkbox" name="diet[]" value="Vegetarian">Vegetarian
+<input type="checkbox" name="diet[]" value="Vegan">Vegan
+<input type="checkbox" name="diet[]" value="Keto">Keto
+<input type="checkbox" name="diet[]" value="Halal">Halal
+
+<br><br>
+
+<input type="submit" name="register" value="Register">
 
 </form>
 
