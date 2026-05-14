b91f38144dd6fe30cb3def5074b17b811cc1a09b
anuanika21 <iamanuanika21@gmail.com>
2026-05-14 13:12:02 +0600

    create registration form with name, username, email, password and diet options

--- register.php
+++ register.php
@@ -6,7 +6,24 @@
 <body>
 
 <h2>User Registration</h2>
+<form method="post" action="registration controller.php">
 
+<input type="text" name="name" placeholder="Name"><br><br>
+
+<input type="text" name="username" placeholder="Username"><br><br>
+
+<input type="email" name="email" placeholder="Email"><br><br>
+
+<input type="password" name="password" placeholder="Password"><br><br>
+
+<label>Dietary Preferences</label><br>
+
+<input type="checkbox" name="diet[]" value="Vegetarian">Vegetarian
+<input type="checkbox" name="diet[]" value="Vegan">Vegan
+<input type="checkbox" name="diet[]" value="Keto">Keto
+<input type="checkbox" name="diet[]" value="Halal">Halal
+
+</form>
 
 
 </form>
