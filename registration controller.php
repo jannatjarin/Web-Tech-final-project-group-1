774dbbdc68eadeab4e9bac2884264fc23b3be37e
anuanika21 <iamanuanika21@gmail.com>
2026-05-14 12:35:58 +0600

    add validation for user registration

--- registration controller.php
+++ registration controller.php
@@ -8,9 +8,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $name = $_POST['name'];
     $username = $_POST['username'];
     $email = $_POST['email'];
+    $password = $_POST['passward'];
 
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+   
     $diet = json_encode($_POST['diet']);
     $role = "user";
-    
+
+   if (empty($username)|| empty($name) || empty($password) || empty($email)) 
+    {
+        $error = "All fields are required!";
+    } 
+    else 
+    {
+        
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
     
