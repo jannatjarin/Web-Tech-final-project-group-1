ea9dd0ed633af6565f14abb13f2fa92a0e25afa6
anuanika21 <iamanuanika21@gmail.com>
2026-05-14 14:12:36 +0600

    insert bookmark record into database with user_id and recipe_id

--- user/bookmarks.php
+++ user/bookmarks.php
@@ -10,6 +10,13 @@ $user_id = $_SESSION['user_id'];
 
 $recipe_id = $_POST['recipe_id']; 
 
-    
+ $sql = "INSERT INTO bookmarks(user_id, recipe_id) VALUES ('$user_id', '$recipe_id')";
+
+if ($conn->query($sql) === TRUE) {
+    $success = "Recipe Saved";
+} else {
+    $error = "Error: " . $conn->error;
+}
+   
 
 ?>
