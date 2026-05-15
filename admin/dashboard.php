7efd5fb26f6674719cbafbbb52e041ffecf608dd
nusratsumu <nusratsumu2003@gmail.com>
2026-05-16 04:23:27 +0600

    Created admin dashboard layout

---
+++ admin/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+    </head>
+
+    <body>
+        <h1>Admin Dashboard</h1>
+
+        <ul>
+            <li><a href=users.php>Manage User</a></li>
+            <li><a href=analytics.php>Analaytics</a></li>
+            <li><a href=settings.php>Settings</a></li>
+
+        </ul>
+    </body>
+</html>
